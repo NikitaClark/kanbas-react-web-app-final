8b17fa04468d769c0e8f44f8ca749dea4785cf64
NikitaClark <nikitaclark296@gmail.com>
2024-10-03 18:52:18 -0400

    s

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,27 +1,26 @@
 import { Link } from "react-router-dom";
+
 export default function Dashboard() {
   return (
     <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12)</h2>
+      <hr />
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
-            <img src="/images/reactjs.jpg" width={200} />
+          <Link className="wd-dashboard-course-link" to="/Kanbas/Courses/1234/Home">
+            {/* <img src="image-url" alt="Course Thumbnail" /> */}
             <div>
-              <h5>
-                 CS1234 React JS
-                 </h5>
+              <h5>CS1234 React JS</h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer
+                Full Stack Software Developer
               </p>
-              <button> Go </button>
+              <button>Go</button>
             </div>
           </Link>
         </div>
-        {/* <div className="wd-dashboard-course"> ... </div>
-        <div className="wd-dashboard-course"> ... </div> */}
+        {/* Additional course entries can go here */}
       </div>
     </div>
   );
